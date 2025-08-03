d876656a13e80d5ca16febd9aa7c3b553ac61036
fabian4 <fabian.v.bao@gmail.com>
2025-08-03 23:04:45 +0800

    init repo

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import DiffClearViewer from "./DiffClearViewer";
+
+function App() {
+
+  return (
+    <div>
+      <DiffClearViewer />
+    </div>
+  )
+}
+
+export default App
