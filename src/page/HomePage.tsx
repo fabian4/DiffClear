b3f79f7bf17f9cf93c2c3662f8ed0baa87b1088c
Fabian Bao <fabian.v.bao@gmail.com>
2025-08-13 17:12:41 +0800

    init repo

--- src/page/HomePage.tsx
+++ src/page/HomePage.tsx
@@ -1,8 +1,9 @@
 import { useState } from "react";
 import { ContentInput } from "../component/ContentInput";
 import { viewModeEnum } from "./HomePage.types";
-import { Button } from "@headlessui/react";
+import { Button, Checkbox, Field, Label, Menu, MenuButton, MenuItem, MenuItems } from "@headlessui/react";
 import { Diff } from "../component/DiffView";
+import { CheckIcon, Cog6ToothIcon, EyeIcon, PencilSquareIcon } from "@heroicons/react/16/solid";
 
 const HomePage = () => {
 
@@ -32,8 +33,8 @@ const HomePage = () => {
 
   return (
     <div className="flex flex-col h-screen">
-      <div className="flex justify-between p-1">
-        <div className="flex gap-5">
+      <div className="grid grid-cols-3 p-1">
+        <div className="flex flex-1 items-center">
           <svg width="36" height="28" viewBox="0 0 36 28" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path d="M6 2.5C6 1.39543 6.89543 0.5 8 0.5H21.1716C21.702 0.5 22.2107 0.710714 22.5858 1.08579L27.4142 5.91421C27.7893 6.28929 28 6.79799 28 7.32843V25.5C28 26.6046 27.1046 27.5 26 27.5H8C6.89543 27.5 6 26.6046 6 25.5V2.5Z" 
                   stroke="#24292e" stroke-width="1.2" stroke-linejoin="round"/>
@@ -48,53 +49,47 @@ const HomePage = () => {
             </g>
           </svg>
         </div>
-        <div className="p-1">
+        <div className="justify-self-center pl-2 p-1">
           <Button 
             onClick={toggleViewMode}
             disabled={viewMode === viewModeEnum.Diff}
             className="inline-flex gap-2 items-center rounded-l-lg disabled:bg-gray-600 disabled:text-white px-5 py-1.5 text-sm/6 font-semibold">
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="size-5">
-              <path d="M10 12.5a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5Z" />
-              <path fillRule="evenodd" d="M.664 10.59a1.651 1.651 0 0 1 0-1.186A10.004 10.004 0 0 1 10 3c4.257 0 7.893 2.66 9.336 6.41.147.381.146.804 0 1.186A10.004 10.004 0 0 1 10 17c-4.257 0-7.893-2.66-9.336-6.41ZM14 10a4 4 0 1 1-8 0 4 4 0 0 1 8 0Z" clipRule="evenodd" />
-            </svg>
+            <EyeIcon className="size-5"/>
             Diff View
           </Button>
           <Button 
             onClick={toggleViewMode}
             disabled={viewMode === viewModeEnum.Edit}
             className="inline-flex gap-2 items-center rounded-r-lg disabled:bg-gray-600 disabled:text-white px-5 py-1.5 text-sm/6 font-semibold">
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="size-5">
-              <path d="m5.433 13.917 1.262-3.155A4 4 0 0 1 7.58 9.42l6.92-6.918a2.121 2.121 0 0 1 3 3l-6.92 6.918c-.383.383-.84.685-1.343.886l-3.154 1.262a.5.5 0 0 1-.65-.65Z" />
-              <path d="M3.5 5.75c0-.69.56-1.25 1.25-1.25H10A.75.75 0 0 0 10 3H4.75A2.75 2.75 0 0 0 2 5.75v9.5A2.75 2.75 0 0 0 4.75 18h9.5A2.75 2.75 0 0 0 17 15.25V10a.75.75 0 0 0-1.5 0v5.25c0 .69-.56 1.25-1.25 1.25h-9.5c-.69 0-1.25-.56-1.25-1.25v-9.5Z" />
-            </svg>
+            <PencilSquareIcon className="size-5"/>
             Edit Mode
           </Button>
         </div>
-        <div className="p-1">  
-          {/* <Field>
-            <Label>Mode </Label>
-            <Switch
-              checked={viewMode === viewModeEnum.Diff}
-              onChange={toggleViewMode}
-              className="group inline-flex h-6 w-11 items-center rounded-full bg-gray-800 transition data-checked:bg-blue-600"
+        <div className="flex-1 justify-self-end p-1">
+          <Menu>
+            <MenuButton className="inline-flex items-center gap-2 rounded-md bg-gray-800 px-3 py-1.5 text-white data-open:bg-gray-900">
+              <Cog6ToothIcon className="size-4" />
+            </MenuButton>
+
+            <MenuItems
+              transition
+              anchor="bottom end"
+              className="w-52 origin-top-right rounded-xl border p-1 text-sm/6 bg-gray-400 transition duration-100 ease-out [--anchor-gap:--spacing(1)] data-closed:scale-95 data-closed:opacity-0"
             >
-              <span className="size-4 translate-x-1 rounded-full bg-white transition group-data-checked:translate-x-6" />
-            </Switch>
-          </Field> */}
-          <Button 
-            className="inline-flex items-center">
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="size-5">
-              <path d="m5.433 13.917 1.262-3.155A4 4 0 0 1 7.58 9.42l6.92-6.918a2.121 2.121 0 0 1 3 3l-6.92 6.918c-.383.383-.84.685-1.343.886l-3.154 1.262a.5.5 0 0 1-.65-.65Z" />
-              <path d="M3.5 5.75c0-.69.56-1.25 1.25-1.25H10A.75.75 0 0 0 10 3H4.75A2.75 2.75 0 0 0 2 5.75v9.5A2.75 2.75 0 0 0 4.75 18h9.5A2.75 2.75 0 0 0 17 15.25V10a.75.75 0 0 0-1.5 0v5.25c0 .69-.56 1.25-1.25 1.25h-9.5c-.69 0-1.25-.56-1.25-1.25v-9.5Z" />
-            </svg>
-          </Button>
-          <Button 
-            className="inline-flex items-center">
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="size-5">
-              <path d="m5.433 13.917 1.262-3.155A4 4 0 0 1 7.58 9.42l6.92-6.918a2.121 2.121 0 0 1 3 3l-6.92 6.918c-.383.383-.84.685-1.343.886l-3.154 1.262a.5.5 0 0 1-.65-.65Z" />
-              <path d="M3.5 5.75c0-.69.56-1.25 1.25-1.25H10A.75.75 0 0 0 10 3H4.75A2.75 2.75 0 0 0 2 5.75v9.5A2.75 2.75 0 0 0 4.75 18h9.5A2.75 2.75 0 0 0 17 15.25V10a.75.75 0 0 0-1.5 0v5.25c0 .69-.56 1.25-1.25 1.25h-9.5c-.69 0-1.25-.56-1.25-1.25v-9.5Z" />
-            </svg>
-          </Button>
+              <MenuItem>
+                <Field className="flex items-center gap-2">
+                  <Checkbox
+                    // checked={enabled}
+                    // onChange={setEnabled}
+                    className="group size-6 rounded-md bg-white/10 p-1 ring-1 ring-white/15 ring-inset focus:not-data-focus:outline-none data-checked:bg-white data-focus:outline data-focus:outline-offset-2 data-focus:outline-white"
+                  >
+                    <CheckIcon className="hidden size-4 fill-black group-data-checked:block" />
+                  </Checkbox>
+                  <Label>Enable beta features</Label>
+                </Field>
+              </MenuItem>
+            </MenuItems>
+          </Menu>
         </div>
       </div>
       {
